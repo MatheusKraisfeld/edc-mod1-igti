f2fadd4d2edb49ea87650c932535bb7904ad6418
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2022-04-01 21:09:28 -0300

    add files to use-case 1

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,28 +1,17 @@
-# HCL - Hashicorp Configuration Language
-# Linguaguem declarativa
-
-resource "aws_s3_bucket" "datalake" {
-  # Parâmetros de configuração do recurso escolhido
-  bucket = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}"
+resource "aws_s3_bucket" "dl" {
+  bucket = "datalake-kraisfeld-igti-edc-tf"
+  acl    = "private"
 
   tags = {
     IES   = "IGTI",
     CURSO = "EDC"
   }
-}
 
-resource "aws_s3_bucket_acl" "datalake" {
-  bucket = aws_s3_bucket.datalake.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "datalake" {
-  bucket = aws_s3_bucket.datalake.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      # kms_master_key_id = data.aws_kms_key.s3.arn
-      sse_algorithm = "AES256"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
   }
 }
