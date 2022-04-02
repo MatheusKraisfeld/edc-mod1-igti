d2d3df09150c389982d11a4f03314e0652da4e2e
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2022-04-01 21:18:39 -0300

    fix test.yaml IaC errors

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,17 +1,24 @@
 resource "aws_s3_bucket" "dl" {
   bucket = "datalake-kraisfeld-igti-edc-tf"
-  acl    = "private"
 
   tags = {
     IES   = "IGTI",
     CURSO = "EDC"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_acl" "dl" {
+  bucket = aws_s3_bucket.dl.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "dl" {
+  bucket = aws_s3_bucket.dl.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      # kms_master_key_id = data.aws_kms_key.s3.arn
+      sse_algorithm = "AES256"
     }
   }
 }
