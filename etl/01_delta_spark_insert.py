ed910913e6221eac87795ab0d78e945937a9ff17
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2022-04-02 19:16:42 -0300

    add upload enem

--- etl/01_delta_spark_insert.py
+++ etl/01_delta_spark_insert.py
@@ -2,10 +2,14 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, min, max
 
 # Cria objeto da Spark Session
-spark = (SparkSession.builder.appName("DeltaExercise")
+spark = (
+    SparkSession.builder.appName("DeltaExercise")
     .config("spark.jars.packages", "io.delta:delta-core_2.12:1.0.0")
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
     .getOrCreate()
 )
 
@@ -24,9 +28,7 @@ enem = (
 # Escreve a tabela em staging em formato delta
 print("Writing delta table...")
 (
-    enem
-    .write
-    .mode("overwrite")
+    enem.write.mode("overwrite")
     .format("delta")
     .partitionBy("year")
     .save("s3://datalake-ney-igti-edc-tf/staging-zone/enem")
