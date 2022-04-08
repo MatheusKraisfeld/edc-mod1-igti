e3564672aa4b245078a4b2581670489cba973dd6
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2022-04-08 19:55:53 -0300

    add kinesis and glue crawler to infrastructure

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -22,3 +22,21 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "dl" {
     }
   }
 }
+
+resource "aws_s3_bucket" "stream" {
+  bucket = "igti-kraisfeld-streaming-bucket"
+  acl    = "private"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
