f6ab632639bc9f07dd13e8c9c8f08d54ce957210
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2022-05-06 08:43:52 -0300

    Add astronomer to install_docker.sh

--- airflow/install_docker.sh
+++ airflow/install_docker.sh
@@ -1,5 +1,10 @@
+echo "Update"
 sudo apt update
+echo "Install docker and docker-compose"
 sudo apt install -y docker.io docker-compose
+echo "Add user to docker group"
 sudo usermod -aG docker ${USER}
+echo "Install astronomer"
+curl -sSL https://install.astronomer.io | sudo bash -s
 echo ""
 echo "Done! Exit the instance and login again, please!"
